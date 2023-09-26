6a2e416c5b129ccbb37148340633474907798d6c
h.khanbeigi <h.khanbeigi@kian.digital>
2023-09-26 16:10:48 +0330

    Initial commit

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const express = require("express");
+// const express = require("express");
+// const fs = require("fs");
+const mammoth = require("mammoth");
 const fs = require("fs");
+// const pdf = require("pdf-parse");
 
-const app = express();
-const port = 3001;
-
-app.get("/read", (req, res) => {
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading file");
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.post("/wirte", (req, res) => {
-  console.log(req);
-  //   const jsonString = JSON.stringify(formattedData, null, 2);
-  //   const filePath = "data.json";
-  //   try {
-  //     fs.writeFileSync(filePath, jsonString, "utf8");
-  //   } catch (err) {
-  //   }
-  res.send("Got a POST request");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const docxFilePath = "./test4.docx";
+const pageNumber = 48;
+// Read the DOCX file
+fs.readFile(docxFilePath, (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
+  mammoth
+    .extractRawText({ buffer: data })
+    .then((result) => {
+      const paragraphs = result.value.split("\n\n");
+      const output = paragraphs
+        .map((paragraph) => ({ text: paragraph })).filter(e=>e.text !== '')
+        
+      const chunkSize = output.length / pageNumber;
+      const res = [];
+      for (let i = 0; i < output.length; i += chunkSize) {
+        const page = output.slice(i, i + chunkSize);
+        res.push({ pages: [{ entities: page }] });
+      }
+      const jsonText = JSON.stringify(res, null, 2);
+      fs.writeFile("output.json", jsonText, (writeErr) => {
+        if (writeErr) {
+          console.error("Error writing JSON file:", writeErr);
+        } else {
+          console.log("JSON data has been written to output.json");
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error converting DOCX to HTML:", error);
+    });
 });
