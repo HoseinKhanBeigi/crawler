aa2079636ce02a3e89c523ee64f10d76e9914d1b
h.khanbeigi <h.khanbeigi@kian.digital>
2023-10-11 15:46:34 +0330

    fix

---
+++ resumedoc.js
@@ -0,0 +1,5 @@
+const res1 = {
+    "led the design and development of multiple enterprice-level applications of bank of krafaring.",
+    "Architected and developed a scalable component library that accelerated frontend development by 30%, leading to more efficient project deliveries and cost savings.",
+    "Implemented a comprehensive automated testing strategy, including unit, integration, and end-to-end testing, which led to a 20% decrease in post-release defects."
+}
